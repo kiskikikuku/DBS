012a4f56e6116b943e2a839e03da126e4758a527
TaeHyun Park <pthkuku99@gmail.com>
2022-12-04 20:30:33 +0900

    final commit

--- LCKdb_CREATE_TABLES.py
+++ LCKdb_CREATE_TABLES.py
@@ -4,35 +4,27 @@ db = pymysql.connect(host='192.168.56.101', user='thpark', password='xogus99', p
 
 cursor = db.cursor()
 
-# sql = ["USE LCK_db;",
-#        "CREATE TABLE player(summoner_ID VARCHAR(20) NOT NULL, firstname VARCHAR(10),lastname VARCHAR(10), line VARCHAR(3), birth INT(8), age INT(2), team VARCHAR(30), salary INT(20),expiration INT(8));",
-#        "SHOW TABLES;"]
-#
-# for s in sql:
-#     cursor.execute(s)
-#     result = cursor.fetchall()
-#     for r in result:
-#         print(r)
+sql = ["USE LCK_db;",
+       "CREATE TABLE player(summoner_ID VARCHAR(20) NOT NULL, firstname VARCHAR(10),lastname VARCHAR(10), line VARCHAR(3), birth INT(8), age INT(2), team VARCHAR(30), salary INT(20),expiration INT(8));",
+       "SHOW TABLES;"]
 
-# sql2 = ["CREATE TABLE club(teamname VARCHAR(30) NOT NULL, initial VARCHAR(3), num_player INT(2), num_coach INT(1));",
-#        "SHOW TABLES;"]
-#
-# sql3 = ["CREATE TABLE coaching_staff(summoner_ID VARCHAR(20) NOT NULL, firstname VARCHAR(10),lastname VARCHAR(10), role VARCHAR(10), birth INT(8), age INT(2), team VARCHAR(30), expiration INT(8));",
-#        "SHOW TABLES;"]
-#
-# for s in sql3:
-#     cursor.execute(s)
-#     result = cursor.fetchall()
-#     for r in result:
-#         print(r)
+for s in sql:
+    cursor.execute(s)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
 
-# sql = "INSERT INTO Book (bookid, bookname, publisher, price) VALUES(%s, %s, %s, %s)"
-#         print("삽입할 책의 정보를 순서대로 입력(bookid, bookname, publisher, price: )")
-#         bookid = int(input())
-#         bookname = str(input())
-#         publisher = str(input())
-#         price = int(input())
-#         cursor.execute(sql, (bookid, bookname, publisher, price))
+sql2 = ["CREATE TABLE club(teamname VARCHAR(30) NOT NULL, initial VARCHAR(3), num_player INT(2), num_coach INT(1));",
+       "SHOW TABLES;"]
+
+sql3 = ["CREATE TABLE coaching_staff(summoner_ID VARCHAR(20) NOT NULL, firstname VARCHAR(10),lastname VARCHAR(10), role VARCHAR(10), birth INT(8), age INT(2), team VARCHAR(30), expiration INT(8));",
+       "SHOW TABLES;"]
+
+for s in sql3:
+    cursor.execute(s)
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
 
 sql = "INSERT INTO club (teamname, initial, num_player, num_coach) VALUES(%s, %s, %s, %s)" # LCK 프랜차이즈 10개팀 입력
 teamlist = [["Hanhwa Life Esports", "HLE", 0, 0],
@@ -52,3 +44,4 @@ for team in teamlist:
     np = team[2]
     nc = team[3]
     cursor.execute(sql, (teamname, initial, np, nc))
+
