17e0d8e5f7fbb87769ab13d180d74674b12dd545
TaeHyun Park <pthkuku99@gmail.com>
2022-12-04 20:13:23 +0900

    ADD INSERT COACH

--- LCKdb.py
+++ LCKdb.py
@@ -29,10 +29,6 @@ while 1:
         team = str(input())
         salary = str(input())
         expiration = str(input())
-        if salary < 60000000:
-            print("최저연봉 조건이 맞지 않습니다.")
-            break
-
         cursor.execute(sql, (id, fname, lname, line, birth, age, team, salary, expiration))
 
     elif command == 2: #선수 제거
@@ -40,44 +36,59 @@ while 1:
         print("Input Summoner ID of player you want delete:  ")
         id = str(input())
         cursor.execute(sql, id)
-        db.commit()
 
     elif command == 3: #선수 검색
-        sql = "SELECT * FROM Book WHERE bookname = %s"
+        sql = "SELECT * FROM player WHERE summoner_ID = %s"
         print("Input Summoner ID of player you want search: ")
         searchPlayer = str(input())
         cursor.execute(sql, searchPlayer)
         result = cursor.fetchall()
+        for data in result:
+            print(data)
 
     elif command == 4: #코칭스태프 추가
-        sql = "INSERT INTO coaching_staff (summoner_ID, firstname, lastname, line, birth, age, team, expiration) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO coaching_staff (summoner_ID, firstname, lastname, role, birth, age, team, expiration) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
         print(
-            "Enter the information of [coachingstaff] you want to insert in order(summoner_ID, firstname, lastname, line, birth, age, team, expiration)")
+            "Enter the information of [coachingstaff] you want to insert in order(summoner_ID, firstname, lastname, role, birth, age, team, expiration)")
         id = str(input())
         fname = str(input())
         lname = str(input())
-        line = str(input())
+        role = str(input())
         birth = str(input())
         age = str(input())
         team = str(input())
         expiration = str(input())
+        cursor.execute(sql, (id, fname, lname, role, birth, age, team, expiration))
+
     elif command == 5: #코칭스태프 제거
-        pass
+        sql = "DELETE FROM coaching_staff WHERE summoner_ID = %s"
+        print("Input Summoner ID of [coaching_staff] you want delete:  ")
+        deleteCoach = str(input())
+        cursor.execute(sql, deleteCoach)
+
     elif command == 6: #코칭스태프 검색
-        pass
+        sql = "SELECT * FROM coaching_staff WHERE summoner_ID = %s"
+        print("Input Summoner ID of [coaching_staff] you want search:  ")
+        searchCoach = str(input())
+        cursor.execute(sql, searchCoach)
+        result = cursor.fetchall()
+        for data in result:
+            print(data)
 
     elif command == 7: #팀 검색
-        sql = "SELECT * FROM player"
-        cursor.execute(sql)
+        sql = "SELECT * FROM player WHERE team = %s"
+        searchteam = str(input())
+        cursor.execute(sql, searchteam)
         result = cursor.fetchall()
         for row_data in result:
             print(row_data)
-        sql2 = "SELECT * FROM coaching_staff"
-        cursor.execute(sql2)
+        sql2 = "SELECT * FROM coaching_staff WHERE team = %s"
+        cursor.execute(sql2, searchteam)
         result = cursor.fetchall()
         for row_data in result:
             print(row_data)
 
     else:
         break
+    db.commit()
 
