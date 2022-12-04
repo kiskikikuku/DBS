1a7d143dd784906a9facb214ecc3855cdae56df5
TaeHyun Park <pthkuku99@gmail.com>
2022-12-04 16:51:42 +0900

    add INSERT player query

--- LCKdb.py
+++ LCKdb.py
@@ -1,27 +1,33 @@
-# import pymysql
-#
-# db = pymysql.connect(host='192.168.56.101', user='thpark', password='xogus99', port=4567, db='LCK_db', charset='utf8')
-#
-# cursor = db.cursor()
+import pymysql
+
+db = pymysql.connect(host='192.168.56.101', user='thpark', password='xogus99', port=4567, db='LCK_db', charset='utf8')
+
+cursor = db.cursor()
 
 while 1:
     print("-----------[2018068040]----------------[Taehyunpark]----------")
     print("[LCK Viewer(E-Sports)]")
-    print("1. Insert Data")
-    print("2. Delete Data")
-    print("3. Search Data")
-    print("4. Print DB")
+    print("1. Add Player")
+    print("2. Add Coaching Staff")
+    print("3. Delete Data")
+    print("4. Search Data")
+    print("5. Print DB")
     print("Others. Exit(Input Any value (except 1,2,3,4)")
     print("--------------------------------------------------------------")
     command = int(input())
     if command == 1:
-        sql = "INSERT INTO Book (bookid, bookname, publisher, price) VALUES(%s, %s, %s, %s)"
-        print("삽입할 책의 정보를 순서대로 입력(bookid, bookname, publisher, price: )")
-        bookid = int(input())
-        bookname = str(input())
-        publisher = str(input())
-        price = int(input())
-        # cursor.execute(sql, (bookid, bookname, publisher, price))
+        sql = "INSERT INTO player (summoner_ID, firstname, lastname, line, birth, age, team, salary, expiration) VALUES(%s, %s, %s, %s)"
+        print("Enter the information of [player] you want to insert in order(summoner_ID, firstname, lastname, line, birth, age, team, salary, expiration)")
+        id = str(input())
+        fname = str(input())
+        lname = str(input())
+        line = str(input())
+        birth = str(input())
+        age = str(input())
+        team = str(input())
+        salary = str(input())
+        expiration = str(input())
+        cursor.execute(sql, (id, fname, lname, line, birth, age, team, salary, expiration))
     elif command == 2:
         sql = "DELETE FROM Book WHERE bookname = %s"
         print("Input Summoner ID you want delete:  ")
